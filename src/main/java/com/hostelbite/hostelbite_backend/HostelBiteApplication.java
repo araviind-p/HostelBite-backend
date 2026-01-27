a7f45be6a5061cea0c8bb83a363a1edd7d9f4032
Aravind P <araviindp@gmail.com>
2026-01-27 21:54:47 +0530

    Initial springboot project setup

---
+++ src/main/java/com/hostelbite/hostelbite_backend/HostelBiteApplication.java
@@ -0,0 +1,13 @@
+package com.hostelbite.hostelbite_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelBiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelBiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hostelbite/hostelbite_backend/HostelBiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostelbite.hostelbite_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelBiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
